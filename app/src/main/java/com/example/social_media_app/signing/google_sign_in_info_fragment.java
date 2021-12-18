e16a81ae01f6ad523ce76eb5169b12cc4945cd0a
naman <Nmndwdi1001@gmail.com>
2021-12-18 22:12:30 +0530

    second commit

---
+++ app/src/main/java/com/example/social_media_app/signing/google_sign_in_info_fragment.java
@@ -0,0 +1,61 @@
+package com.example.social_media_app.signing;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.social_media_app.MainActivity;
+import com.example.social_media_app.R;
+import com.example.social_media_app.databasing.databasing_write;
+import com.example.social_media_app.databinding.FragmentGoogleSignInInfoFragmentBinding;
+
+public class google_sign_in_info_fragment extends Fragment {
+
+    FragmentGoogleSignInInfoFragmentBinding binding;
+    String gender="";
+
+    public google_sign_in_info_fragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding= FragmentGoogleSignInInfoFragmentBinding.inflate(inflater, container, false);
+        View view =binding.getRoot();
+
+        String userid= getArguments().getString("userid");
+        String email= getArguments().getString("email");
+
+        binding.proceed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username=binding.username.getText().toString();
+                String fullname=binding.fullname.getText().toString();
+                int age= Integer.parseInt(binding.age.getText().toString());
+                int checked_gender_id=binding.gender.getCheckedRadioButtonId();
+                String st_gen=Integer.toString(checked_gender_id);
+                if(st_gen.equals("1"))
+                {
+                    gender="Boys";
+                }
+                else if(st_gen.equals("2"))
+                {
+                    gender="Girls";
+                }
+                databasing_write databasing_write=new databasing_write(username,fullname,email,gender,userid,age);
+                databasing_write.create_account_google();
+                Intent intent=new Intent(getContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
+    }
+}
--- app/src/main/java/com/example/social_media_app/signing/signup_fragment.java
+++ app/src/main/java/com/example/social_media_app/signing/signup_fragment.java
@@ -7,7 +7,6 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,24 +14,19 @@ import android.widget.Toast;
 
 import com.example.social_media_app.MainActivity;
 import com.example.social_media_app.R;
-import com.example.social_media_app.databasing;
+import com.example.social_media_app.databasing.databasing_write;
 import com.example.social_media_app.databinding.FragmentSignupFragmentBinding;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
 
 
 public class signup_fragment extends Fragment {
     FragmentSignupFragmentBinding binding;
+    String gender="";
 
     public signup_fragment() {
         // Required empty public constructor
@@ -48,9 +42,6 @@ public class signup_fragment extends Fragment {
             login_fragment login_fragment=new login_fragment();
             FragmentTransaction transaction=getParentFragmentManager().beginTransaction();
 
-            String username=getArguments().getString("username");
-            String fullname=getArguments().getString("fullname");
-
             binding.login.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -63,14 +54,31 @@ public class signup_fragment extends Fragment {
             binding.signUp.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    auth.createUserWithEmailAndPassword(binding.Email.getText().toString(),binding.Password.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    String email=binding.Email.getText().toString();
+                    String password=binding.Password.getText().toString();
+                    String fullname=binding.fullname.getText().toString();
+                    String username=binding.username.getText().toString();
+                    int age= Integer.parseInt(binding.age.getText().toString());
+                    int checked_gender_id=binding.gender.getCheckedRadioButtonId();
+                    String st_gen=Integer.toString(checked_gender_id);
+                    if(st_gen.equals("1"))
+                    {
+                        gender="Boys";
+                    }
+                    else if(st_gen.equals("2"))
+                    {
+                        gender="Girls";
+                    }
+                    auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if(task.isSuccessful())
                             {
                                 Toast.makeText(getContext(), "user created successfully", Toast.LENGTH_SHORT).show();
-                                databasing databasing=new databasing(username,fullname,binding.Email.getText().toString(),binding.Password.getText().toString());
-                                databasing.create_account();
+                                FirebaseUser user=auth.getCurrentUser();
+                                String userid=user.getUid();
+                                databasing_write databasing_write=new databasing_write(username,fullname,email,password,gender,age,userid);
+                                databasing_write.create_account();
                                 Intent intent=new Intent(getContext(), MainActivity.class);
                                 startActivity(intent);
                             }
@@ -83,17 +91,16 @@ public class signup_fragment extends Fragment {
                 }
             });
 
-            GoogleSignInClient googleSignInClient =username_fragment.getInstance().create_request();
-            binding.google.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    username_fragment.getInstance().signIn(googleSignInClient);
-                }
-            });
+//            GoogleSignInClient googleSignInClient =username_fragment.getInstance().create_request();
+//            binding.google.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    username_fragment.getInstance().signIn(googleSignInClient);
+//                }
+//            });
 
             return view;
     }
-
 
 
 
